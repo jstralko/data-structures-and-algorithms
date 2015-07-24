23f08ea89ff2c0ec28270cccd3258344c7e0e6a3
Gerb Stralko <gerb.stralko@gmail.com>
2015-07-23 21:29:41 -0400

    init root node working

--- rbtree.c
+++ rbtree.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 enum node_color { RED, BLACK };
 
@@ -9,6 +10,8 @@ struct node {
 	enum node_color color;
 	int value;
 };
+
+struct node *rb_root;
 
 struct node *grandparent(struct node *n)
 {
@@ -29,7 +32,33 @@ struct node *uncle(struct node *n)
 	return g->left;
 }
 
+struct node *create_new_rb_node(int value)
+{
+	struct node *n;
+       
+	n = (struct node *)malloc(sizeof(*n));
+	if (n == NULL) {
+		printf("Failed to create new rb node\n");
+		return NULL;
+	}
+	n->color = RED;
+	n->value = 1;
+
+	return n;
+}
+
+void init_rb_tree(int value)
+{
+	rb_root = create_new_rb_node(1);
+	if (rb_root == NULL)
+		return;
+	rb_root->color = BLACK;
+	rb_root->value = value;
+}
+
 int main(int argv, char *argc[])
 {
+	init_rb_tree(1);
+
 	return 0;
 }
