b47d7c4f78be3c9617e6172fe0aab80911c8a0ba
Gerald J Stralko <gerald.stralko@wolterskluwer.com>
2016-09-08 09:03:19 -0400

    Java implementation of a largest sum subtree

---
+++ treeTotal.java
@@ -0,0 +1,36 @@
+4
+2          -10
+1   1    3      0
+
+
+public class NodeWrapper {
+  Integer max;
+  Node subTreeNode;
+}
+
+public static void start() {
+  NodeWrapper maxSubTreeWrapper = new NodeWrapper();
+  maxSubTreeWrapper.max = 0;
+  maxSubTreeWrapper.subTreeNode = null;
+  setLargestSumTree(root, maxSubTreeWrapper);
+}
+
+public static Integer setLargestSumTree(Node root, NodeWrapper maxSubTreeWrapper) {
+  if (root == null) return 0;
+
+  int total = root.value;
+  if (root.left != null) {
+    total += setLargestSumTree(root.left, maxSubTreeWrapper);
+  }
+
+  if (root.right != null) {
+    total += setLargestSumTree(root.right, maxSubTreeWrapper);
+  }
+
+  if (maxSubTreeWrapper.max < total) {
+    maxSubTreeWrapper.max = total;
+    maxSubTreeWrapper.subTreeNode = root;
+  }
+
+  return total;
+}
